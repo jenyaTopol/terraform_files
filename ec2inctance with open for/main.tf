2814f1bf9ab97e13b52c6252b08a984e0b8dddcf
JenyTopol <51214133+jenyaTopol@users.noreply.github.com>
2021-11-06 14:20:26 +0200

    Update main.tf

--- ec2inctance with open for/main.tf
+++ ec2inctance with open for/main.tf
@@ -37,12 +37,14 @@ ingress {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }    
    ingress {
     from_port   = 0
-    to_port     = 0
+    to_port     = 65535
     protocol    = "-1"
-    cidr_blocks      = ["0.0.0.0/0"] 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
 
  }
 
