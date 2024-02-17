ba88203d92667b984c9edb91083642cded56723f
jenyatopol <jeka010@walla.com>
2024-02-17 16:39:22 +0200

    openvpn-terraform

---
+++ openvpn/main.tf
@@ -0,0 +1,24 @@
+#my terraform
+#buil by jenyatopol
+resource "aws_instance" "openvpn_server" {
+  ami           = "ami-0abcd1234abcd1234" # change ami
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = "your-key-pair" # Change to your key pair
+
+  # Security group for OpenVPN server
+  vpc_security_group_ids = [aws_security_group.openvpn_sg.id]
+
+  tags = {
+    Name = "OpenVPNServer"
+  }
+}
+
+resource "aws_security_group" "openvpn_sg" {
+  name        = "openvpn-sg"
+  description = "Security group for OpenVPN server"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  # Add rules for OpenVPN server
+
+  
